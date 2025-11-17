55751a21930eaac3869668f6d653adc8caeb5363
nyxar77 <153492661+nyxar77@users.noreply.github.com>
2025-11-17 03:21:26 +0100

    Revert "new features:"

--- scripts/app.ts
+++
@@ -1,9 +0,0 @@
-window.addEventListener("urlchange", function() {
-  document.querySelectorAll<HTMLAnchorElement>(".nav-links li a").forEach(element => {
-    if (element.getAttribute("path") === window.location.pathname) {
-      element.classList.add("active");
-    } else {
-      element.classList.remove("active")
-    }
-  });
-})
